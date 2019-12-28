c159e7b58bbb68f0cf525a17a059745c03f6b60d
Pawel <pawel_kantor@hotmail.com>
2019-12-28 18:15:07 +0100

    oth: implemented cars table

--- app/src/main/java/jo/carinfo/ConfigManager.kt
+++ app/src/main/java/jo/carinfo/ConfigManager.kt
@@ -1,75 +1,110 @@
 package jo.carinfo
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 import android.widget.Toast
 import java.io.*
 import com.google.gson.GsonBuilder
 
-const val CARS_FILE = "cars.txt"
 
-class ConfigManager(private var context: Context) {
+class ConfigManager(private var ctx: Context): SQLiteOpenHelper(ctx, DATABASE_NAME, null, DATABASE_VERSION) {
 
-    fun getCars(): CarsList
-    {
-        Log.d("CFGMGR", "getting cars")
-        var result = CarsList()
-        val fileObj = File(context.filesDir, CARS_FILE)
+    override fun onCreate(p0: SQLiteDatabase) {
+        val create_table = ("CREATE TABLE 'cars' ('id' INTEGER PRIMARY KEY," +
+                            "'name' VARCHAR(45) NULL);")
+        p0.execSQL(create_table)
+    }
 
-        if (fileObj.exists())
-        {
-            val gson = GsonBuilder().setPrettyPrinting().create()
-            try
-            {
-                Log.d("CFGMGR", "cars file exists, reading...")
-                result = gson.fromJson<CarsList>(fileObj.readText(), CarsList::class.java)
-                Log.d("CFGMGR", "got " + result.count().toString() + " cars from file")
-            }catch(e: Exception)
-            {
-                val builder = StringBuilder()
-                builder.append(R.string.couldNotGetCars)
-                builder.append(" - ")
-                builder.append(e.message)
-                Toast.makeText(context, builder.toString(), Toast.LENGTH_SHORT).show()
-            }
+    override fun onUpgrade(p0: SQLiteDatabase, oldVer: Int, newVer: Int) {
+        p0.execSQL("DROP TABLE IF EXISTS 'cars'")
+        onCreate(p0)
+    }
+
+    fun addCar(aCarName: String): Boolean {
+        val values = ContentValues()
+        val db = this.writableDatabase
+        try {
+            values.clear()
+            values.put("name", aCarName)
+            val _success = db.insert("cars", null, values)
+            return Integer.parseInt("$_success") != -1
+        } finally {
+            db.close()
         }
-        else
-            Log.d("CFGMGR", "cars file does not exists")
+    }
 
+    fun editCarName(aOldCarName: String, aNewName: String): Boolean {
+        val values = ContentValues()
+        val db = this.writableDatabase
+        try {
+            values.put("name", aNewName)
+            val _success = db.update("cars", values, "name=?", arrayOf(aOldCarName))
+            return Integer.parseInt("$_success") != -1
+        } finally {
+            db.close()
+        }
+    }
+
+    fun removeCar(aCarName: String): Boolean {
+        val db = this.writableDatabase
+        try {
+            val _success = db.delete("cars", "name=?",  arrayOf(aCarName))
+            return Integer.parseInt("$_success") != -1
+        } finally {
+            db.close()
+        }
+    }
+
+    fun removeCars(aCars: CarsList): Boolean {
+        val values = ContentValues()
+        val db = this.writableDatabase
+        try {
+            for (car in aCars)
+                db.delete("cars", "name=?",  arrayOf(car.mName))
+            return true
+        } finally {
+            db.close()
+        }
+    }
+
+    fun saveCars(aCars: CarsList): Boolean {
+        val values = ContentValues()
+        var _success: Long
+        val db = this.writableDatabase
+        try {
+            for (car in aCars)
+            {
+                values.clear()
+                values.put("name", car.mName)
+                _success = db.insert("cars", null, values)
+                if (Integer.parseInt("$_success") == -1)
+                    return false
+            }
+            return true
+        } finally {
+            db.close()
+        }
+    }
+
+    fun getAllCars(): CarsList {
+        val db = this. readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM 'cars'", null)
+        val result = CarsList()
+        if (cursor.moveToFirst())
+        {
+            result.add(Car(cursor.getString(cursor.getColumnIndex("name"))))
+            while (cursor.moveToNext())
+                result.add(Car(cursor.getString(cursor.getColumnIndex("name"))))
+        }
+        cursor.close()
         return result
     }
 
-    fun saveCars(aCarsList: CarsList): Boolean {
-        val fileObj = File(context.filesDir, CARS_FILE)
-        try {
-            if (!fileObj.parentFile.exists())
-                fileObj.parentFile.mkdirs()
-
-            if (!fileObj.exists())
-                fileObj.createNewFile()
-
-            Log.d("CFGMGR", "saving " + aCarsList.count().toString() + " cars to file")
-
-            val f = FileOutputStream(fileObj)
-            val sw = OutputStreamWriter(f)
-            val gson = GsonBuilder().setPrettyPrinting().create()
-            try {
-                sw.write(gson.toJson(aCarsList))
-                Log.d("CFGMGR", "cars saved")
-            } catch (e: Exception)
-            {
-                Log.e("CFGMGR", String.format("cars not saved, %s", e.message))
-                return false
-            } finally {
-                sw.close()
-                f.close()
-            }
-
-            return true
-        } catch (e: FileNotFoundException) {
-            Log.e("CFGMGR", e.message)
-            return false
-        }
+    companion object {
+        const val DATABASE_NAME = "carsdb"
+        const val DATABASE_VERSION = 1
     }
-
 }
