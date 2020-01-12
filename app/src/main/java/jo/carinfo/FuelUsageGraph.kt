0153c12f1711a1e125465dfafbf0cd24c9cba991
Pawel <pawel_kantor@hotmail.com>
2020-01-12 17:27:24 +0100

    add: change graph visualization type, visualising cost per liter and total cost of fuel

--- app/src/main/java/jo/carinfo/FuelUsageGraph.kt
+++ app/src/main/java/jo/carinfo/FuelUsageGraph.kt
@@ -3,21 +3,30 @@ package jo.carinfo
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter
 import com.jjoe64.graphview.series.*
+import org.joda.time.DateTime
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
 class FuelUsageGraph : AppCompatActivity() {
 
+    private val TAG = "GRAPH"
     private val mCars = CarsListGraph()
+    private var mChartType = ChartType.FUEL_USAGE
     private lateinit var mAdapter: CarGraphAdapter
+    private lateinit var mFuelGraph: GraphView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,32 +39,42 @@ class FuelUsageGraph : AppCompatActivity() {
                     mCars.add(car)
             }
 
+        mFuelGraph = findViewById(R.id.gvFuel)
+        initGraph()
+        val spinner = findViewById<Spinner>(R.id.sChartTypes)
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                return
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Log.i(TAG, "selected $p2 item, refreshing series")
+                when (p2) {
+                    0 -> refreshSeries(ChartType.FUEL_USAGE)
+                    1 -> refreshSeries(ChartType.COST_PER_LITER)
+                    2 -> refreshSeries(ChartType.TOTAL_COST)
+                    else -> throw NotImplementedError("chart type $p2 not implemented")
+                }
+            }
+        }
+        spinner.adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, resources.getStringArray(R.array.ChartTypes))
+
         showCars()
     }
 
-    private fun clearGraphPoints() {
-        val fuel_graph = findViewById<GraphView>(R.id.gvFuel)
-        fuel_graph.removeAllSeries()
+    private fun initGraph() {
+        mFuelGraph.gridLabelRenderer.numHorizontalLabels = 4
+        mFuelGraph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(this, SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()))
     }
 
-    private fun showGraphPoints() {
+    private fun clearGraphPoints() {
+        mFuelGraph.removeAllSeries()
+    }
+
+    private fun showFuelUsageOnGraph() {
         var series: LineGraphSeries<DataPoint>
-        val fuel_graph = findViewById<GraphView>(R.id.gvFuel)
-        var maxY = 10.0
-
-        fuel_graph.gridLabelRenderer.horizontalAxisTitle = "Data"
-        fuel_graph.gridLabelRenderer.verticalAxisTitle = "Spalanie"
-        fuel_graph.gridLabelRenderer.numHorizontalLabels = 4
-        fuel_graph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(this, SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()))
-
-        for (car in mCars) {
-            if (!mCars.isCarHidden(car)) {
-                for (entry in car.mFuelEntries)
-                if (maxY < entry.getAvgFuelConsumption())
-                    maxY = entry.getAvgFuelConsumption()
-            }
-        }
-        fuel_graph.viewport.setMaxY(maxY)
+        mFuelGraph.gridLabelRenderer.verticalAxisTitle = resources.getString(R.string.FuelConsumptionChart)
 
         for (car in mCars) {
             if (!mCars.isCarHidden(car)) {
@@ -71,14 +90,88 @@ class FuelUsageGraph : AppCompatActivity() {
                 for (single_entry in car.mFuelEntries)
                     series.appendData(DataPoint(single_entry.mDate, single_entry.getAvgFuelConsumption()), true, 1000)
 
-                fuel_graph.addSeries(series)
+                mFuelGraph.addSeries(series)
             }
         }
-        fuel_graph.viewport.isXAxisBoundsManual = true
-        fuel_graph.viewport.isScalable = true
-        fuel_graph.viewport.isScrollable = true
     }
 
+    private fun showTotalCostOnGraph() {
+        var series: LineGraphSeries<DataPoint>
+        mFuelGraph.gridLabelRenderer.verticalAxisTitle = resources.getString(R.string.TotalCostChart)
+
+        for (car in mCars) {
+            if (!mCars.isCarHidden(car)) {
+                car.mFuelEntries.sortByDate()
+                series = LineGraphSeries()
+                series.isDrawDataPoints = true
+                series.color = car.mChartColor
+                series.setOnDataPointTapListener(object: OnDataPointTapListener {
+                    override fun onTap(series: Series<*>?, dataPoint: DataPointInterface?) {
+                        Log.d("series", String.format("%f, %.2f", dataPoint?.x, dataPoint?.y))
+                    }
+                })
+                for (single_entry in car.mFuelEntries)
+                    series.appendData(DataPoint(single_entry.mDate, single_entry.mPerLiter * single_entry.mFuelAmount), false, 1000)
+
+                mFuelGraph.addSeries(series)
+            }
+        }
+    }
+
+    private fun showCostPerLiterOnGraph() {
+        var series: LineGraphSeries<DataPoint>
+        mFuelGraph.gridLabelRenderer.verticalAxisTitle = resources.getString(R.string.CostPerLiterChart)
+
+        for (car in mCars) {
+            if (!mCars.isCarHidden(car)) {
+                car.mFuelEntries.sortByDate()
+                series = LineGraphSeries()
+                series.isDrawDataPoints = true
+                series.color = car.mChartColor
+                series.setOnDataPointTapListener(object: OnDataPointTapListener {
+                    override fun onTap(series: Series<*>?, dataPoint: DataPointInterface?) {
+                        Log.d("series", String.format("%f, %.2f", dataPoint?.x, dataPoint?.y))
+                    }
+                })
+                for (single_entry in car.mFuelEntries)
+                    series.appendData(DataPoint(single_entry.mDate, single_entry.mPerLiter), false, 1000)
+
+                mFuelGraph.addSeries(series)
+            }
+        }
+    }
+
+    private fun addPointAtTheEnd() {
+
+        var maxX = 0.0
+        var maxY = 0.0
+        for (simpleSeries in mFuelGraph.series) {
+            if (maxX < simpleSeries.highestValueX)
+                maxX = simpleSeries.highestValueX
+            if (maxY < simpleSeries.highestValueY)
+                maxY = simpleSeries.highestValueY
+        }
+        val newDate = DateTime(maxX.toLong()).plusWeeks(1)
+
+        val series = LineGraphSeries<DataPoint>()
+        series.isDrawDataPoints = false
+        series.appendData(DataPoint(newDate.millis.toDouble(), maxY), false, 1)
+        mFuelGraph.addSeries(series)
+    }
+
+    private fun showGraphPoints() {
+        when (mChartType) {
+            ChartType.FUEL_USAGE -> showFuelUsageOnGraph()
+            ChartType.COST_PER_LITER -> showCostPerLiterOnGraph()
+            ChartType.TOTAL_COST -> showTotalCostOnGraph()
+            else -> throw NotImplementedError("cannot show graph points for $mChartType")
+        }
+        addPointAtTheEnd()
+        mFuelGraph.viewport.isXAxisBoundsManual = true
+        mFuelGraph.viewport.isScalable = true
+        mFuelGraph.viewport.isScrollable = true
+        mFuelGraph.viewport.scrollToEnd()
+    }
 
     private fun showCars()
     {
@@ -89,8 +182,7 @@ class FuelUsageGraph : AppCompatActivity() {
         mAdapter.let {
             it.onItemClick = { it, isChecked ->
                 mAdapter.changeItemVisibility(it, isChecked)
-                clearGraphPoints()
-                showGraphPoints()
+                refreshSeries(mChartType)
             }
         }
 
@@ -98,9 +190,15 @@ class FuelUsageGraph : AppCompatActivity() {
         listView.adapter = mAdapter
     }
 
+    private fun refreshSeries(aChartType: ChartType) {
+        mChartType = aChartType
+        clearGraphPoints()
+        showGraphPoints()
+    }
+
     override fun onBackPressed() {
         val drawer = findViewById<DrawerLayout>(R.id.dlFuelUsage)
-        if (drawer.isDrawerOpen(Gravity.START))
+        if (drawer.isDrawerOpen(GravityCompat.START))
             drawer.closeDrawers()
         else
             super.onBackPressed()
